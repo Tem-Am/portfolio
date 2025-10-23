56978ef776fd296c69528eecac6e399d487a9d85
------- <tamarjargal25@my.whitworth.edu>
2025-10-22 21:20:55 -0700

    Deleted. Start again!

--- README.md
+++
@@ -1,3 +0,0 @@
-# React + Vite
-
-Hello, Welcome to Tem's portfolia. In this website, you can see my some cool project and little bit about me.
--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const words = [
-    {text: "About Me", imgPath: ""},
-    {text: "Experience", imgPath: ""},
-    {text: "Projects", imgPath: ""}
-]
